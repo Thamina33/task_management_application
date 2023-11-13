fe392d3076b0fdfb5a2daf7a327a7489deda2d7b
Thamina Omar Toma <omartoma33@gmail.com>
2023-11-13 19:56:12 +0600

    Initial commit

---
+++ lib/ui/screens/progress_tasks_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../widget/profile_summary_card.dart';
+import '../widget/task_item_card.dart';
+
+class ProgressTasksScreen extends StatefulWidget {
+  const ProgressTasksScreen({super.key});
+
+  @override
+  State<ProgressTasksScreen> createState() => _ProgressTasksScreenState();
+}
+
+class _ProgressTasksScreenState extends State<ProgressTasksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              const ProfileSummaryCard(),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return const TaskItemCard();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
